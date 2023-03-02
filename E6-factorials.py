0c2ccc2472e72a4621d2659c0fbfc6ec8896621a
Mike Gillis <mikegillis22@gmail.com>
2023-03-02 11:23:53 -0500

    Added input prompt and output string

--- E6-factorials.py
+++ E6-factorials.py
@@ -3,17 +3,19 @@ Write a Python program to calculate the factorial of a number using a while loop
 
 Example: If the input number is 5, the program should output 120 (which is the product of 5 x 4 x 3 x 2 x 1).
 """
-n = 6
+numberEntered = input("Enter a number to calculate its factorial => ")
+n = int(numberEntered)
 numbers = []
 z = 0
 while n > 0:
     numbers.append(n)
     n = n - 1
-print(numbers)
+#print(numbers)
 y = len(numbers) - 1
 x = numbers[z]
 while y > 0:
     z = z + 1
     x = x * numbers[z]
-    print(x)
-    y = y - 1    
+    #print(x)
+    y = y - 1
+print("The factorial of", numberEntered, "is", x, ".")     
